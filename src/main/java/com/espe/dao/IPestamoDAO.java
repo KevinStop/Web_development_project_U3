208c58da89069a19eeb2894a34ea3a94ad7f92e1
AlexSG <84851263+SteveenStopper@users.noreply.github.com>
2023-08-31 17:43:10 -0500

    Libros Prestamos

--- src/main/java/com/espe/dao/IPestamoDAO.java
+++ src/main/java/com/espe/dao/IPestamoDAO.java
@@ -1,4 +1,17 @@
 package com.espe.dao;
 
+import com.espe.model.Prestamo;
+
+import java.util.List;
+
 public interface IPestamoDAO {
+    List<Prestamo> getAllPrestamos();
+
+    Prestamo getPrestamoById(int id);
+
+    void addPrestamo(Prestamo prestamo);
+
+    void updatePrestamo(Prestamo prestamo);
+
+    void deletePrestamo(int id);
 }
